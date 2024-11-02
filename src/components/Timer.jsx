8bd09d24355d08cf6a5c2b3411e8b044dfb11ddf
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-11-02 07:49:33 -0700

    remove localstorage to lift up later, add toggle

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -5,8 +5,7 @@ const TIMER_STATES = { ACTIVE: "active", PAUSED: "paused", FINISHED: 'finished' 
 const rooster = new Audio('../../public/rooster.wav')
 
 function Timer({ initialSeconds, title, handleChildTimerFinish }) {
-    const localInitialSeconds = JSON.parse(localStorage.getItem('seconds')) || initialSeconds
-    const [seconds, setSeconds] = useState(localInitialSeconds)
+    const [seconds, setSeconds] = useState(initialSeconds)
     const [timerState, setTimerState] = useState(TIMER_STATES.PAUSED)
     const timerRef = useRef(null)
 
@@ -21,7 +20,6 @@ function Timer({ initialSeconds, title, handleChildTimerFinish }) {
             clearInterval(timerRef.current)
             timerRef.current = null
             setTimerState(TIMER_STATES.FINISHED)
-            localStorage.removeItem('seconds')
             rooster.play()
         }
     }, [seconds])
@@ -30,7 +28,6 @@ function Timer({ initialSeconds, title, handleChildTimerFinish }) {
         if (timerRef.current === null) {
             timerRef.current = setInterval(() => {
                 setSeconds(prev => {
-                    localStorage.setItem('seconds', prev - 1)
                     setTimerState(TIMER_STATES.ACTIVE)
                     return prev - 1
                 })
--- src/components/TimerContainer.jsx
+++ src/components/TimerContainer.jsx
@@ -5,7 +5,8 @@ const TIMER_STATES = { WORK: "work", REST: "rest" }
 
 function TimerContainer() {
     const [timerState, setTimerState] = useState(TIMER_STATES.WORK)
-    const handleChildTimerFinish = () => {
+
+    const toggleWorkRestState = () => {
         if (timerState === TIMER_STATES.WORK) {
             setTimerState(TIMER_STATES.REST)
         }
@@ -14,13 +15,18 @@ function TimerContainer() {
         }
     }
 
+    const handleChildTimerFinish = () => {
+        toggleWorkRestState()
+    }
+
     return (
         <div>
             <p>current state {JSON.stringify(timerState)}</p>
+            <p>Toggle <button onClick={toggleWorkRestState}>{timerState}</button></p>
             {timerState === TIMER_STATES.WORK ?
-                <Timer initialSeconds={3} title={"Work"} handleChildTimerFinish={handleChildTimerFinish} />
+                <Timer key="work" initialSeconds={3} title={"Work"} handleChildTimerFinish={handleChildTimerFinish} />
                 :
-                <Timer initialSeconds={2} title={"Rest"} handleChildTimerFinish={handleChildTimerFinish} />
+                <Timer key="rest" initialSeconds={2} title={"Rest"} handleChildTimerFinish={handleChildTimerFinish} />
             }
         </div>
     )
